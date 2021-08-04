084fb27bffb459e671dfeff5c459a3939e3c61af
batuhanileri <bileriofficial@gmail.com>
2021-08-04 15:31:07 +0300

    Pull data, simple frontend

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,32 @@
+﻿using DataAccessLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager
+    {
+        GenericRepository<Category> repo = new GenericRepository<Category>();
+   
+        public List<Category> GetAll()
+        {
+            return repo.GetAll();
+        }
+        public void CategoryAddBL(Category p)
+        {
+            if(p.CategoryName=="" || p.CategoryName.Length<=2||
+               p.CategoryDescription=="")
+            {
+                //hata mesajı
+            }
+            else
+            {
+                repo.Add(p);
+            }
+        }
+    }
+}
---
+++ Project_MVC/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project_MVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        CategoryManager cm = new CategoryManager();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetCategoryList()
+        {
+            var category_values = cm.GetAll();
+            return View(category_values);
+
+        }
+    }
+}
