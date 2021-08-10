3cf1c6c1d5ab9a870c225e4859ffc2427dd132a8
batuhanileri <bileriofficial@gmail.com>
2021-08-10 14:43:34 +0300

    added Message layer Context Modified set interface

---
+++ BusinessLayer/FluentValidation/MessageValidator.cs
@@ -0,0 +1,28 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.FluentValidation
+{
+    public class MessageValidator : AbstractValidator<Message>
+    {
+        public MessageValidator()
+        {
+
+
+            RuleFor(x => x.ReceiverMail).NotEmpty().WithMessage("Maili  boş geçemezsiniz.");
+            RuleFor(x => x.Subject).NotEmpty().WithMessage("Konu  boş geçemezsiniz.");
+            RuleFor(x => x.MessageContent).NotEmpty().WithMessage("Mesajı  boş geçemezsiniz.");
+            RuleFor(x => x.Subject).MinimumLength(3).WithMessage("Konu Başlığı en az 3 karakter olmalı");
+            RuleFor(x => x.ReceiverMail).EmailAddress().WithMessage("Geçerli bir e-mail giriniz");
+
+        }
+        
+    }
+    }
+
---
+++ Project_MVC/Controllers/MessageController.cs
@@ -0,0 +1,37 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project_MVC.Controllers
+{
+    public class MessageController : Controller
+    {
+        // GET: Message
+        MessageManager mm = new MessageManager(new EfMessageDal());
+        public ActionResult Inbox()
+        {
+            var messageList = mm.GetListInbox();
+            return View(messageList);
+        }
+        public ActionResult Sendbox()
+        {
+            var messageList = mm.GetListSendbox();
+            return View(messageList);
+        }
+        [HttpGet]
+        public ActionResult NewMessage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult NewMessage(Message message)
+        {
+            return View();
+        }
+    }
+}
